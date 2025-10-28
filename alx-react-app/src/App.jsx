f4745dc2ae536c149ba4e1558aa5d4bfacaef5e9
ekewubas@gmail.com <you@example.com>
2025-10-28 17:59:53 +0100

    Added Header, MainContent, and Footer components and integrated into App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,13 +1,15 @@
-import React from 'react';
-import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <WelcomeMessage />
-    </div>
+    <>
+      <Header />
+      <MainContent />
+      <Footer />
+    </>
   );
 }
 
 export default App;
-
