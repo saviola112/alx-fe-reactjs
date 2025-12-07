79df928b68fcde86d08472b5f0a27a5bc2d9793f
ekewubas@gmail.com <you@example.com>
2025-12-07 12:06:55 +0100

    Fix Tailwind setup and empty README

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,12 @@
-module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
-  darkMode: false,
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-};
+}
+
